65341d6bba0e5fd27e90297a6980e2b8e94f70a1
M6K4N9R <zubchevskiy@gmail.com>
2024-06-10 10:51:34 +0200

    REMOVED THE PROJECT WITH APP ROUTER

--- app/api/about/route.js
+++
@@ -1,9 +0,0 @@
-import dbConnect from "@/db/dbConnect";
-import Treatment from "@/db/models/Treatment";
-import { NextResponse } from "next/server";
-
-export default async function GET() {
-  await dbConnect();
-  const treatments = await Treatment.find();
-  return NextResponse.json({ treatments });
-}
--- app/test/[slug]/page.jsx
+++
@@ -1,4 +0,0 @@
-export default function DynamicPage({ params }) {
-  console.log("Params: ", params);
-  return <h2>Hello, Dynamic {params.slug}</h2>;
-}
