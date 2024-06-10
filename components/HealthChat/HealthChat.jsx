bdc24b1dbb0bb7c41f78ba90216e9b9919d163e7
M6K4N9R <zubchevskiy@gmail.com>
2024-06-10 17:57:56 +0200

    Start with Questionaire Container

---
+++ components/HealthChat/HealthChat.jsx
@@ -0,0 +1,25 @@
+import { Box } from "@mui/material";
+import { Button } from "@mui/material";
+
+export default function HealthChatInvite() {
+  return (
+    <Box
+      height={200}
+      width={400}
+      my={4}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      gap={4}
+      p={2}
+      sx={{ border: "2px solid grey", borderRadius: "9px" }}
+    >
+      <h2>Tell us how you feel</h2>
+      <p>
+        Here you can fill in our health related questionaire and get suggestion
+        on naturopathic treatments
+      </p>
+      <Button variant="contained">Start questionaire</Button>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import TreatmentsList from "@/components/TreatmentsList/TreatmentsList";
 import AuthButton from "@/components/auth-button/AuthButton";
+import HealthChatInvite from "@/components/HealthChat/HealthChat";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,13 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <AuthButton />
+      <section className={`flex justify-between`}>
+        <h3>Good morning. What can we do for you?</h3>
+        <AuthButton />
+      </section>
+      <h2>Better Holistic.</h2>
+      <p>Your naturopathic practice in Berlin.</p>
+      <HealthChatInvite />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Yet another try&nbsp;
