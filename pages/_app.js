64af7765528b0025004e767fbd4cac5ce2e93b6f
M6K4N9R <zubchevskiy@gmail.com>
2024-06-10 16:21:20 +0200

    Login with github (Google still nedd working on)

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import "@/styles/globals.css";
 import { SWRConfig } from "swr";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <SWRConfig
-      value={{ fetcher: (...args) => fetch(...args).then((res) => res.json()) }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
+    <SessionProvider session={session}>
+      <SWRConfig
+        value={{
+          fetcher: (...args) => fetch(...args).then((res) => res.json()),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </SessionProvider>
   );
 }
