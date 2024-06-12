490d0f547eef101ee96220728e02452e6a013336
M6K4N9R <zubchevskiy@gmail.com>
2024-06-12 18:43:42 +0200

    Works but with a bug

--- components/DetailedTreatment/DetailedTreatment.jsx
+++ components/DetailedTreatment/DetailedTreatment.jsx
@@ -29,7 +29,7 @@ export default function DetailedTreatment() {
   const router = useRouter();
   const { slug } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/treatments/${slug}`);
+  const { data, isLoading } = useSWR("/api/treatments/");
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -38,9 +38,31 @@ export default function DetailedTreatment() {
   if (!data) {
     return;
   }
+  const treatmentsSlugsArray = data
+    ?.map((treatment) => treatment.slug)
+    .filter((treatment) => treatment !== "first-consultation");
+
+  const currentTreatment = data?.find((treatment) => treatment.slug === slug);
+
+  const indexOfCurrentTreatment = treatmentsSlugsArray?.indexOf(
+    currentTreatment?.slug
+  );
+  let indexOfNextTreatment;
+
+  if (indexOfCurrentTreatment < treatmentsSlugsArray?.length) {
+    indexOfNextTreatment = indexOfCurrentTreatment + 1;
+  } else {
+    indexOfNextTreatment = 0;
+  }
+
+  console.log("currentTreatment ", currentTreatment);
+  console.log("treatmentSlugs ", treatmentsSlugsArray);
+  console.log("indexOfCurrentTreatment ", indexOfCurrentTreatment);
 
   function handleNext() {
-    console.log("Went to next treatment");
+    const nextTreatment = treatmentsSlugsArray[indexOfNextTreatment];
+    router.push(`/treatments/${nextTreatment}`);
+    console.log("Went to next treatment", nextTreatment);
   }
   function handlePrev() {
     console.log("Went to prev treatment");
@@ -59,24 +81,25 @@ export default function DetailedTreatment() {
       </div>
       <div className="flex flex-col items-start mb-5">
         <Image
-          src={data.image}
-          alt={data.name}
+          alt={currentTreatment.name}
+          src={currentTreatment.image}
           width={1920}
           height={1080}
           className="w-full h-[70vh] object-cover md:w-[90vw] lg:w-[45vw]"
         />
         <div className="px-3">
-          <h2>{data.name}</h2>
-          <p className="white-space-pre-wrap">{data.text}</p>
+          <h2>{currentTreatment.name}</h2>
+          <p className="white-space-pre-wrap">{currentTreatment.text}</p>
         </div>
 
         <StyledSymptomsBox>
           <h3>Symptoms Addressed</h3>
           <p className="white-space-pre-wrap mb-4">
-            {`You can consider booking`} <strong>{`${data.name}`}</strong>{" "}
+            {`You can consider booking`}{" "}
+            <strong>{`${currentTreatment.name}`}</strong>{" "}
             {`when you have the following symptoms:`}
           </p>
-          {data.symptoms.map((symptom, index) => (
+          {currentTreatment.symptoms.map((symptom, index) => (
             <div
               className="grow px-2 py-1 h-8 text-center align-middle rounded-lg bg-secondary text-white"
               key={index}
@@ -86,7 +109,7 @@ export default function DetailedTreatment() {
           ))}
         </StyledSymptomsBox>
         <div className="self-center">
-          <StyledButton>{`Book ${data.name}`}</StyledButton>
+          <StyledButton>{`Book ${currentTreatment.name}`}</StyledButton>
         </div>
       </div>
     </>
