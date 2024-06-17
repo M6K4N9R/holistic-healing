4a25fd3492e8da5e78a39599edb11c1b0908f5da
M6K4N9R <zubchevskiy@gmail.com>
2024-06-17 16:22:11 +0200

    Update Booking Schema

--- db/models/Booking.js
+++ db/models/Booking.js
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const bookingSchema = new Schema({
-  treatment: { type: String, required: true },
-  doctor: { type: String, required: true },
-  doctorID: { type: String, required: true },
+  treatment: { type: [Schema.Types.ObjectId], ref: "Treatment" },
+  doctor: { type: [Schema.Types.ObjectId], ref: "Doctor" },
 });
 
 const Booking =
