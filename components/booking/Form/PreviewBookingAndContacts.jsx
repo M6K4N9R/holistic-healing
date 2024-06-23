7eeb26f3cee3493335a885e85a23adba8cd7f147
M6K4N9R <zubchevskiy@gmail.com>
2024-06-23 22:34:07 +0200

    Change submit button. Add class name from Preview Booking module.css

--- components/booking/Form/PreviewBookingAndContacts.jsx
+++ components/booking/Form/PreviewBookingAndContacts.jsx
@@ -1,4 +1,8 @@
-import { previewBookingAndContacts } from "./PreviewBookingAndContacts.module.css";
+import {
+  previewBookingAndContacts,
+  submit,
+  edit,
+} from "./PreviewBookingAndContacts.module.css";
 import { StyledButton } from "@/components/DefaulButton/DefaultButton";
 
 export default function PreviewBookingAndContacts({
@@ -14,9 +18,8 @@ export default function PreviewBookingAndContacts({
   return (
     <>
       <div className={previewBookingAndContacts}>
-        <div className="text-center text-white pb-5  border-b-2 border-bright">
+        <div className="text-center text-white border-b-2 border-bright">
           <h3>Please review your booking</h3>
-          <p className="-mt-6">and provide us with your contact details.</p>
         </div>
         <div className="text-center mt-6 ">
           <h4 className="text-white font-semibold">
@@ -34,57 +37,64 @@ export default function PreviewBookingAndContacts({
               {selectedTimeSlot?.timeSlot}
             </span>
           </h4>
-          <div className="mb-4">
-            <label
-              htmlFor="full-name"
-              className="block text-left text-sm font-medium text-gray-300"
-            >
-              Your full name
-            </label>
-            <input
-              id="full-name"
-              name="full-name"
-              type="text"
-              onChange={onHandlePatientNameInput}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
-            />
-          </div>
-          <div className="mt-6 mb-4">
-            <label
-              htmlFor="contact-number"
-              className="block text-left text-sm font-medium text-gray-300"
-            >
-              phone
-            </label>
-            <input
-              id="contact-number"
-              name="contact-number"
-              type="tel"
-              onChange={onHandleContactNumberInput}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-left text-sm font-medium text-gray-300"
-            >
-              email
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={onHandleEmailInput}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
-            />
+          <div className="mt-7 border-2 border-bright p-3 rounded-lg ">
+            <p className="-mt-6  border-primary rounded-lg bg-primary mx-1 text-wrap text-bright">
+              Please fill your contact details.
+            </p>
+            <div className="mb-4 mt-3">
+              <label
+                htmlFor="full-name"
+                className="block text-left text-sm font-medium text-gray-300"
+              >
+                Your name
+              </label>
+              <input
+                id="full-name"
+                name="full-name"
+                type="text"
+                onChange={onHandlePatientNameInput}
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
+              />
+            </div>
+            <div className="mt-6 mb-4">
+              <label
+                htmlFor="contact-number"
+                className="block text-left text-sm font-medium text-gray-300"
+              >
+                Phone
+              </label>
+              <input
+                id="contact-number"
+                name="contact-number"
+                type="tel"
+                onChange={onHandleContactNumberInput}
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-left text-sm font-medium text-gray-300"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={onHandleEmailInput}
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
+              />
+            </div>
           </div>
         </div>
         <div className="text-center mx-auto my-6">
-          <StyledButton type="submit">Book an appointment</StyledButton>
+          <button type="submit" className={submit}>
+            Book an appointment
+          </button>
         </div>
       </div>
     </>
