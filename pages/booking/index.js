634105b70c777674f5775a3c4d5644abf8f61e6b
M6K4N9R <zubchevskiy@gmail.com>
2024-06-25 14:29:38 +0200

    Delete comments in pages/booking

--- pages/booking/index.js
+++ pages/booking/index.js
@@ -85,28 +85,7 @@ export default function BookingTreatmentsList() {
         "doctorsWhoOfferSelectedTreatment: ",
         doctorsWhoOfferSelectedTreatment
       );
-      // if (
-      //   excistingBookingsWithSelectedTreatment.length > 0 &&
-      //   selectedDate?.date
-      // ) {
-      //   const excistingBookingsWithSelectedTreatmentOnSelectedDate =
-      //     excistingBookingsWithSelectedTreatment?.filter(
-      //       (booking) => booking.date.date === selectedDate.date
-      //     );
-      //   console.log(
-      //     "On same date bookings: ",
-      //     excistingBookingsWithSelectedTreatmentOnSelectedDate
-      //   );
-      //   if (
-      //     excistingBookingsWithSelectedTreatmentOnSelectedDate.length > 0 &&
-      //     selectedTimeSlot?.isSelected === true
-      //   ) {
-      //     const doctorsWhoOfferSelectedTreatment =
-      //       excistingBookingsWithSelectedTreatmentOnSelectedDate?.filter(
-      //         (booking) => booking.time === selectedTimeSlot.timeSlot
-      //       );
-      //   }
-      // }
+
       console.log("Selected date: ", selectedDate);
     }
   }, [selectedTreatment, data, selectedDate, selectedTimeSlot]);
@@ -156,21 +135,6 @@ export default function BookingTreatmentsList() {
     availableTimeSlots.push(doctorBloodloverData.availability);
   }
 
-  /* 
-  ----------------- Filtering timeSlots options based on selected Treatment
-  
-  const excistingBookingsWithSelectedTreatment = [{}] // 1. Go through bookings and return bookings who's booking.treatment === selectedtreatment
-  const excistingBookingsWithSelectedDate = excistingBookingsWithSelectedTreatment.map((booking)=> booking.date.date.includes(selectedDate.date)) // 2. 
-  const timeSlotIsAlreadyBookedBooking = excistingBookingsWithSelectedDate.map((booking) => booking.time === selectedTimeSlot)
-  const doctorOfMatchedTreatment = timeSlotIsAlreadyBookedBooking.doctor
-  const otherDoctorsWhoCanDoThisTreatment = doctors.map((doctor) => doctor.availability.includes(selectedTimeSlot))
-  const doctorsThatAreNotBookedOnThisDayAndTime = bookings.map((booking) => )
-    if (timeSlotIsAlreadyBooked?.length > 0 && ) {
-      
-    }
-    
-
-*/
   // ===================== HANDLING SELECTION and CLEARING SELECTION FUNCTIONS
 
   // ----------------- Select/Clear Treatment
@@ -239,23 +203,6 @@ export default function BookingTreatmentsList() {
   };
 
   // ====================================== HANDLING SUBMIT =========================
-
-  // const formFirstValidation = () => {
-  //   if (
-  //     !selectedTreatment ||
-  //     !selectedDate ||
-  //     !selectedTimeSlot ||
-  //     !selectedDoctor
-  //   ) {
-  //     setFormError(
-  //       "Please make sure you selected a treatment, date and time and doctor."
-  //     );
-  //     return false;
-  //   }
-
-  //   setFormError("");
-  //   return true;
-  // };
 
   const resetForm = () => {
     setSelectedTreatment();
