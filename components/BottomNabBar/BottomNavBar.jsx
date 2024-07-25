cc365fd47298b9de9fa44c4b47a4cc3cb8362a50
M6K4N9R <zubchevskiy@gmail.com>
2024-07-25 06:47:32 +0200

    Update Navbar to use CSS Module approach

--- components/BottomNabBar/BottomNavBar.jsx
+++ components/BottomNabBar/BottomNavBar.jsx
@@ -2,8 +2,19 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "./BottomNavBar.module.css";
 
+const aboutPathnames = [
+  "/treatments/ear-accupuncture",
+  "/treatments/reiki",
+  "/treatments/fussreflexzonenmassage",
+  "/treatments/schropfen",
+  "/treatments/blutegeltherapie",
+  "/treatments/drip-infusion-therapy",
+];
+
 const BottomNavBar = () => {
   const router = useRouter();
+
+  console.log("Pathname", router.pathname);
 
   return (
     <nav className={styles.navbar}>
@@ -11,7 +22,11 @@ const BottomNavBar = () => {
         <div className={router.pathname === "/" ? styles.active : ""}>Home</div>
       </Link>
       <Link href="/treatments/ear-accupuncture">
-        <div className={router.pathname === "/treatments" ? styles.active : ""}>
+        <div
+          className={
+            aboutPathnames.includes(router.pathname) ? styles.active : ""
+          }
+        >
           About
         </div>
       </Link>
