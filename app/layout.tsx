e9b7266ac9bd1d7445f47e808552482bad0ee3a4
M6K4N9R <zubchevskiy@gmail.com>
2025-12-17 22:04:01 +0100

    Refacture Start. Change to Typescript and App router. Raplace jsconfig with new tsconfig.json. Move all pages to app/. Rename index.js to page.ts and add layout.ts

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { SWRConfig } from 'swr';
+import { SessionProvider } from 'next-auth/react';
+import LayoutNavBar from '@/components/LayoutNavBar';
+import './globals.css'; // Your global styles
+import { Grechen_Fuemen } from 'next/font/google';
+
+const grechen = Grechen_Fuemen({ 
+  weight: "400", 
+  subsets: ["latin"],
+  variable: '--font-grechen'
+});
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={grechen.variable}>
+      <body>
+        <SWRConfig value={{ fetcher }}>
+          <SessionProvider>
+            <LayoutNavBar>
+              {children}
+            </LayoutNavBar>
+          </SessionProvider>
+        </SWRConfig>
+      </body>
+    </html>
+  );
+}
