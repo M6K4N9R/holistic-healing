1945b063994cc094ff4b62de05a249e8eecd0b55
M6K4N9R <zubchevskiy@gmail.com>
2025-12-19 14:57:51 +0100

    Add types/booking.ts

---
+++ types/booking.ts
@@ -0,0 +1,26 @@
+export interface Treatment {
+  _id: string;
+  name: string;
+  slug: string;
+  price: number;
+  duration: number;
+}
+
+export interface Doctor {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  treatments: string[]; // treatment IDs
+}
+
+export interface BookingFormData {
+  treatmentId: string;
+  treatmentName: string;
+  doctorId: string;
+  doctorName: string;
+  date: Date;
+  timeSlot: string;
+  patientName: string;
+  email: string;
+  phone: string;
+}
