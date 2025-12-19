5d5703af05ea66d6a49b748937e0d33d040fe25d
M6K4N9R <zubchevskiy@gmail.com>
2025-12-19 15:15:48 +0100

    Remove Block treatment and the other

--- components/BlockTreatment/BlockTreatment.jsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image"
-import styles from "./BlockTreatment.module.css"
-
-export default function BlockTreatment({ image }) {
-    return <div className={styles.container}>
-        <Image alt='' src={image.src} fill={true} style={styles.image} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" priority={true}/>
-    </div>
-}
