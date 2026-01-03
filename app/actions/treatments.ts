6a83a332535e0d818aeb6db3b1708af5e65d17f2
M6K4N9R <zubchevskiy@gmail.com>
2026-01-03 21:25:25 +0100

    Remove actions/booking.tsx

--- app/actions/treatments.ts
+++ app/actions/treatments.ts
@@ -4,6 +4,6 @@ import Treatment from "@/db/models/Treatment";
 
 export async function getTreatments() {
   await dbConnect();
-  const treatments = await Treatment.find({}).limit(12).lean();
+  const treatments = await Treatment.find({}).lean();
   return { treatments };
 }
