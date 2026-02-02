6efdd0e7870680b0aae0bd44c902b44d53b93750
M6K4N9R <zubchevskiy@gmail.com>
2026-02-02 10:47:03 +0100

    create new route api/availability/[treatmentId]

---
+++ app/api/availability/[treatmentId]/route.ts
@@ -0,0 +1,10 @@
+import { getAvailability } from "@/app/actions/booking-flow";
+
+export async function GET(request: Request, { params }: { params: { treatmentId: string } }) {
+  const url = new URL(request.url);
+  const date = url.searchParams.get("date") || undefined;
+  const location = url.searchParams.get("location") || undefined;
+  
+  const data = await getAvailability(params.treatmentId, { date, location });
+  return Response.json(data);
+}
